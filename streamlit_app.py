9d27639833dbce3b543ada9795eaed663f3b6947
Bala Kiruthika Lakshmi R <109142823+balakiruthikalakshmir@users.noreply.github.com>
2022-07-12 17:23:25 +0530

    Update streamlit_app.py
    added emoji header

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,3 +10,5 @@ st.header('Breakfast Menu')
 st.text('Omega 3 & Blueberry Oatmeal')
 st.text('Kale, Spinach & Rocket Smoothie')
 st.text('Hard-Boiled Free-Range Egg')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
