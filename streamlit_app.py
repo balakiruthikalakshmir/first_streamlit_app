87f4f1617473465d9cd4f00ba6aa0e42ca5d9951
Bala Kiruthika Lakshmi R <109142823+balakiruthikalakshmir@users.noreply.github.com>
2022-07-12 16:10:06 +0530

    Update streamlit_app.py
    replaced double quotes with single quotes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 
-st.title("My first streamlit title")
-st.title("My first streamlit header")
-st.title("My first streamlit text")
-st.title("My second streamlit text")
+st.title('My first streamlit title')
+st.title('My first streamlit header')
+st.title('My first streamlit text')
+st.title('My second streamlit text')
