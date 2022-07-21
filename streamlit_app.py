11f5fa8ea6e0f156b861f096dfcc07cff495e34a
Bala Kiruthika Lakshmi R <109142823+balakiruthikalakshmir@users.noreply.github.com>
2022-07-21 16:25:49 +0530

    Update streamlit_app.py
    adding code to get fruitname as input

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,9 @@ st.dataframe(fruits_to_show)
 
 
 st.header("Fruityvice Fruit Advice!")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruit_choice = st.text_input('What fruit would you like information about?','Kiwi')
+st.write('The user entered ', fruit_choice)
+#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 #st.text(fruityvice_response.json())
 # takes the json version of response and normalize it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
