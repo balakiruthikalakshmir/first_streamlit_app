b5939bbca3fd166ec27b999915be3b6d4fc4ec42
Bala Kiruthika Lakshmi R <109142823+balakiruthikalakshmir@users.noreply.github.com>
2022-07-25 16:39:34 +0530

    Update streamlit_app.py
    added new text box - What fruit would you like to add

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,3 +45,6 @@ my_cur.execute("SELECT * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 st.header("fruit load list contains:")
 st.dataframe(my_data_rows)
+
+fruit_choice = st.text_input('What fruit would you like to add?','jackfruit')
+st.write('Thanks for adding ', fruit_choice)
